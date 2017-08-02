e5007d8b1f99e52bcdb1c79b0d7de58f08039bb1
JaxomofRuatha <jchritton33@gmail.com>
2017-08-02 18:29:26 -0500

    Icon working but needs async fix, package needs componentWillUpdate

--- src/components/CurrentWeatherDisplay.js
+++ src/components/CurrentWeatherDisplay.js
@@ -1,73 +1,83 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
-  const iconOptions = (currentIcon) => {
-    switch (currentIcon) {
-      case "clear-day": return {
-        icon: 'CLEAR_DAY',
-        color: '#fce637',
-      };
-      case "clear-night": return {
-        icon: 'CLEAR_NIGHT',
-        color: '#FFFFFF',
-      };
-      case "partly-cloudy-day": return {
-        icon: 'PARTLY_CLOUDY_DAY',
-        color: '#FFFDED',
-      };
-      case "partly-cloudy-night": return {
-        icon: 'PARTLY_CLOUDY_NIGHT',
-        color: '#edf4ff',
-      };
-      case "cloudy": return {
-        icon: 'CLOUDY',
-        color: '#edf4ff',
-      };
-      case "rain": return {
-        icon: 'RAIN',
-        color: '#91bbff',
-      };
-      case "sleet": return {
-        icon: 'SLEET',
-        color: '#7d99c6',
-      };
-      case "snow": return {
-        icon: 'SNOW',
-        color: '#c2cad6',
-      };
-      case "wind": return {
-        icon: 'CLEAR_DAY',
-        color: 'goldenrod',
-      };
-      case "fog": return {
-        icon: 'FOG',
-        color: '#cdd1d6',
-      };
-    }
+const iconOptions = (currentIcon) => {
+  switch (currentIcon) {
+    case "clear-day": return {
+      icon: 'CLEAR_DAY',
+      color: '#fce637',
+    };
+    case "clear-night": return {
+      icon: 'CLEAR_NIGHT',
+      color: '#FFFFFF',
+    };
+    case "partly-cloudy-day": return {
+      icon: 'PARTLY_CLOUDY_DAY',
+      color: '#FFFDED',
+    };
+    case "partly-cloudy-night": return {
+      icon: 'PARTLY_CLOUDY_NIGHT',
+      color: '#edf4ff',
+    };
+    case "cloudy": return {
+      icon: 'CLOUDY',
+      color: '#edf4ff',
+    };
+    case "rain": return {
+      icon: 'RAIN',
+      color: '#91bbff',
+    };
+    case "sleet": return {
+      icon: 'SLEET',
+      color: '#7d99c6',
+    };
+    case "snow": return {
+      icon: 'SNOW',
+      color: '#c2cad6',
+    };
+    case "wind": return {
+      icon: 'CLEAR_DAY',
+      color: 'goldenrod',
+    };
+    case "fog": return {
+      icon: 'FOG',
+      color: '#cdd1d6',
+    };
+  }
 };
 const CurrentWeatherDisplay = ({ currentWeather }) => {
   let currentIconOptions = iconOptions(currentWeather.currentIcon) || { "icon": "PARTLY_CLOUDY_DAY", "color": "goldenrod" };
   console.log(currentIconOptions);
-    return (
-      <div>
-        <div id="weather-summary"> 
+  return (
+    <div>
+      <div id="weather-summary">
+        <div id="weather-icon">
           <ReactAnimatedWeather icon={currentIconOptions.icon} color={currentIconOptions.color} size={150} animate={true} />
-          <h1>{currentWeather.currentSummary}</h1>
-          <h1>{Math.round(currentWeather.currentTemp)}</h1>
-          <button>Change temperature type!</button>
         </div>
+        <h1 id="quick-sum">{currentWeather.currentSummary}</h1>
+        <h1 id="temp">{Math.round(currentWeather.currentTemp)}&#8457;</h1>
+        <button>&#8457;/&#8451;</button>
+      </div>
+      <div id="weather-lower">
         <div id="weather-misc">
           <p>
             Wind: {currentWeather.currentWind} mph
-            Humidity: {currentWeather.currentHumidity * 100}%
-            Visibility: {currentWeather.currentVisibility || "Miles"} miles
-          </p>
+              Humidity: {currentWeather.currentHumidity * 100}%
+              Visibility: {currentWeather.currentVisibility} miles
+            </p>
         </div>
         <div>
           <p>{currentWeather.currentDaySummary}</p>
         </div>
       </div>
-    );
+    </div>
+  );
+}
+
+/*CurrentWeatherDisplay.defaultProps = {
+  "currentWeather": {
+    "currentIcon": "clear_day",
   }
+}*/
 
 export default CurrentWeatherDisplay;
--- src/components/DayOutlookDisplay.js
+++ src/components/DayOutlookDisplay.js
@@ -4,7 +4,7 @@ export class DayOutlookDisplay extends React.Component {
   render() {
     return (
       <div>
-        <p>{this.props.hour}:{this.props.minute}</p>
+        <p>TODO</p>
       </div>
     );
   }
